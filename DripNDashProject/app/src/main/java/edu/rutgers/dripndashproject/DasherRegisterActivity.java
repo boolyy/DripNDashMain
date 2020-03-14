bad121ed666f0b2aa7b89533be72c173cc711315
boolyy <abdullahbashir7@gmail.com>
2020-03-14 10:39:14 -0400

    Commited Test activity
    Not a real activity, just to test if you're connected to firebase

---
+++ DripNDashProject/app/src/main/java/edu/rutgers/dripndashproject/DasherRegisterActivity.java
@@ -0,0 +1,42 @@
+package edu.rutgers.dripndashproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class DasherRegisterActivity extends AppCompatActivity {
+    Button randomButton;
+    FirebaseAuth firebaseAuthorizer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dasher_register);
+        randomButton = findViewById(R.id.button);
+
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuthorizer = FirebaseAuth.getInstance();
+                firebaseAuthorizer.createUserWithEmailAndPassword("liamTest@gmail.com","password")
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(DasherRegisterActivity.this, "Account Created", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+            }
+        });
+
+
+
+    }
+}
