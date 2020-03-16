73f14914ac566b6d87f342b0c2f7977e7859d3a5
boolyy <55770860+boolyy@users.noreply.github.com>
2020-03-16 17:55:15 -0400

    Added to register Activity
    Programmed buttons to go to dasher or customer register

--- DripNDashProject/app/src/main/java/edu/rutgers/dripndashproject/RegisterActivity.java
+++ DripNDashProject/app/src/main/java/edu/rutgers/dripndashproject/RegisterActivity.java
@@ -2,7 +2,9 @@ package edu.rutgers.dripndashproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -15,6 +17,21 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         customerRegisterButton = findViewById(R.id.button7);
         dasherRegisterButton = findViewById(R.id.button8);
+
+        customerRegisterButton.setOnClickListener(new View.OnClickListener() { //sets up listener for customer register button
+            @Override
+            public void onClick(View v) { //runs when customer register button is clicked
+                startActivity(new Intent(RegisterActivity.this, CustomerRegisterActivity.class));
+            }
+        });
+
+        dasherRegisterButton.setOnClickListener(new View.OnClickListener() {//sets up listener for dasher reg button
+            @Override
+            public void onClick(View v) { //runs when dash reg button is clicked
+                startActivity((new Intent(RegisterActivity.this, DasherRegisterActivity.class)));
+            }
+        });
+
 
 
 
