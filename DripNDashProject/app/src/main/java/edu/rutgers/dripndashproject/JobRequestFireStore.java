25049e2034744e486a714e6d96045c3f1796d9dc
MrNicolasR <60662162+MrNicolasR@users.noreply.github.com>
2020-03-25 21:32:53 -0400

    Add files via upload
    Implemented interface stuff (please check to make sure it works did it differently). Removed Toast but kept Success/Failure listener for another function to write errors.

--- DripNDashProject/app/src/main/java/edu/rutgers/dripndashproject/JobRequestFireStore.java
+++ DripNDashProject/app/src/main/java/edu/rutgers/dripndashproject/JobRequestFireStore.java
@@ -13,6 +13,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,7 +41,9 @@ public class JobRequestFireStore extends JobRequest{
     }
 
 
-    // Write to database (Need to figure activities)
+
+
+    // Write to job request that are pending assignment to dorm collection
     private void writeJobRequest(JobRequest jobRequest){
 
         // Hash map to write data into the database
@@ -59,20 +63,17 @@ public class JobRequestFireStore extends JobRequest{
         Document.put("WAS_CANCELLED", jobRequest.wasCancelled);
 
         // Write to the database
-        db.collection("jobsInProgress").document().set(jobRequest)
+        db.collection("dormName").document("jobPendingAssignment").set(jobRequest.jobID)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Toast.makeText(CustomerHomeActivity.this,
-                                "Created", Toast.LENGTH_SHORT).show();
+
                     }
                 }) // Checks to see if it failed
                 .addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                // Show Toast
-                Toast.makeText(CustomerHomeActivity.this,
-                        "Created", Toast.LENGTH_SHORT).show();;
+
             }
         });
 
@@ -86,43 +87,128 @@ public class JobRequestFireStore extends JobRequest{
             @Override
             // If Job was deleted
             public void onSuccess(Void aVoid) {
-                Toast.makeText(CustomerHomeActivity.this,
-                        "Job Deleted", Toast.LENGTH_SHORT).show();
+
             }// If Job couldn't be deleted
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(CustomerHomeActivity.this,
-                        "Error!", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, e.toString());
+
             }
         });
 
     }
 
-    // Update a job's status
-    private void updateJobStatus(JobRequest jobRequest, String Status) {
+    // Update a job's status // Error
+    private void updateJobStatus(JobRequest jobRequest, int CurrentStage) {
         // Document Reference
         DocumentReference job = db.collection("jobsInProgress").document(jobRequest.jobID);
         // Update the FireBase object in the database
-        job.update("STATUS", Status).addOnSuccessListener(new OnSuccessListener<Void>() {
+        job.update("CURRENT_STAGE", currentStage).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             // If Job was deleted
             public void onSuccess(Void aVoid) {
                 // Check to see if the status was update
-                Toast.makeText(CustomerHomeActivity.this,
-                        "Job Status Updated", Toast.LENGTH_SHORT).show();
+
             }// If Job couldn't be deleted
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(CustomerHomeActivity.this,
-                        "Error Updating the Job Status!", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, e.toString());
+
             }
         });
+
         // Update the local JobRequest Object
-        jobRequest.Status = Status;
+        jobRequest.currentStage = currentStage;
+    }
+
+    // Update on AssignmentAccept
+    private void onAssignmentAccept(JobRequest jobRequest) {
+        // Document Reference
+        DocumentReference job = db.collection("jobsInProgress").document(jobRequest.jobID);
+        // Get the time/date
+        Date currentTime = Calendar.getInstance().getTime();
+        // Update the FireBase object in the database
+        job.update("ASSIGNED_TIMESTAMP", currentTime).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            // If Job was deleted
+            public void onSuccess(Void aVoid) {
+                // Check to see if the status was update
+
+                // Write to inProgressJobs
+                moveToInProgressJobs(jobRequest);
+            }// If Job couldn't be deleted
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        // Update the local JobRequest Object
+        jobRequest.assignedTimestamp = assignedTimestamp;
+    }
+
+    // Move document from dorm collection into inProgressJob collection
+    // Move jobs in progress to completed jobs
+    private void moveToInProgressJobs(final JobRequest jobRequest){
+
+        // From path
+        final DocumentReference fromDoc = db.collection("dorms").
+                document("dormName").collection("jobsPendingAssignment")
+                .document(jobRequest.jobID);
+        // To Path
+        final DocumentReference toDoc = db.collection("jobsInProgress")
+                .document(jobRequest.jobID);
+
+        fromDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                // Check to see if successful
+                if(task.isSuccessful()){
+                    final DocumentSnapshot document = task.getResult();
+                    if(document != null){
+                        toDoc.set(document.getData()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // Transfer document
+                                Log.d(TAG, "Document Transferred");
+                                // Delete the job
+                                fromDoc.delete()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Document successfully deleted");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            // Can't delete document from the jobsInProgress
+                                            // Collection
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error deleting document", e);
+                                            }
+                                        });
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    // If the document can't be transferred
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error writing document",e);
+
+                                    }
+                                });
+                    }else{
+                        // If the document doesn't exist
+                        Log.d(TAG, "No such document");
+                    }
+
+                }else{
+                    // If the process fails
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
     // Get the oldest Job in the database (NEEDS to be Tested)
@@ -140,29 +226,45 @@ public class JobRequestFireStore extends JobRequest{
     }
 
     // getInProgressJob and convert it to a jobRequest object
-    // Having problems returning the JobRequest object
-    private void getInProgressJob(JobRequest jobRequest){
+    JobRequest getInProgressJob(final JobRequest jobRequest){
 
-        // Document Reference to get the document from the database
-        DocumentReference docRef = db.collection("jobsInProgress").document(jobRequest.jobID);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        // Create new JobRequest instance
+        final JobRequest inProgressJobRequest = new JobRequest();
+
+        // Get document reference
+        DocumentReference documentReference = db.collection("jobsInProgress").document(jobRequest.jobID);
+
+        // Starting transferring data
+        documentReference.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        JobRequest newDoc = document.toObject(JobRequest.class);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                inProgressJobRequest.jobID = jobID;
+                inProgressJobRequest.customerUID = customerUID;
+                inProgressJobRequest.currentStage = currentStage;
+                inProgressJobRequest.Status = Status;
+                inProgressJobRequest.wasCancelled = wasCancelled;
+                inProgressJobRequest.amountPaid = amountPaid;
+                inProgressJobRequest.assignedTimestamp = assignedTimestamp;
+                inProgressJobRequest.customerInstructions = customerInstructions;
+                inProgressJobRequest.customerRating = customerRating;
+                inProgressJobRequest.customerReview = customerReview;
+                inProgressJobRequest.dasherName = dasherName;
+                inProgressJobRequest.dasherRating = dasherRating;
+                inProgressJobRequest.dasherUID = dasherUID;
+                inProgressJobRequest.dorm = dorm;
+                inProgressJobRequest.dormRoom = dormRoom;
+                inProgressJobRequest.customerName = customerName;
+                inProgressJobRequest.machineCost = machineCost;
+                inProgressJobRequest.numLoadsActual = numLoadsActual;
+                inProgressJobRequest.numLoadsEstimate = numLoadsEstimate;
+                inProgressJobRequest.requestTimestamp = requestTimestamp;
+
             }
         });
 
-
+        // Return inProgressJobRequest instance
+        return inProgressJobRequest;
     }
 
     // Cancel the job
